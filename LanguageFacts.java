ff4d8a85e3d0ca9c4e69994d39c95f05531d56f2
pujithas7 <pujithasatyanarayana7@gmail.com>
2025-07-20 22:37:28 +0530

    Add files via upload

---
+++ LanguageFacts.java
@@ -0,0 +1,10 @@
+public class LanguageFacts {
+  public static void main(String[] args) {
+    // Press enter or return on your keyboard after each semicolon!
+    System.out.print("Programming is the process of setting instructions to tell the computer how to perform a specofic task or solving a problem........");
+    
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes have a 'main' method.");
+    System.out.println("Java statements end with a semicolon.");
+  }
+}
